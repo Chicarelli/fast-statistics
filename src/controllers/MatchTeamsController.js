b1ffbfd9ea76099d5f0901e64c4ba25a4a2834be
Rafael Chicarellil <rafaelsep20@gmail.com>
2021-08-20 21:07:42 -0300

    Adicionado verificação se há partidas no dia e atualizando status de partidas procuradas para 1.

--- src/controllers/MatchTeamsController.js
+++ src/controllers/MatchTeamsController.js
@@ -2,31 +2,48 @@ const searchMatchTeams = require('../puppeteers/searchMatchTeams');
 const MatchTeams = require("../models/Match_teams");
 const Match = require('../models/Match');
 
-const findMatchesTeamBasedOnMatch = async() => {
-  const data = await Match.findAll();
-  
-  // data.map(item => {
-  //   console.log(item.home_team, item.away_team, item.campeonato);
-  // })
+const findMatchesTeamBasedOnMatch = async () => {
+  /* 
+   * status = 0; Jogos que ainda não foram procurados. 
+   */
+  const data = await Match.findAll({
+    where: {
+      status: 0
+    }
+  });
 
-  for(const i in data){
+  if(data.length == 0){
+    console.log('Não existem partidas!');
+    return;
+  }
+
+  for (const i in data) {
     // console.log(data[i].home_team)
     const result = await searchMatchTeams(data[i].home_team, data[i].away_team, data[i].campeonato)
-    console.log(data[i].campeonato, result);
 
-    await createMatchTeam(data[i].id, data[i].campeonato ,result);
+    await createMatchTeam(data[i].id, data[i].campeonato, result);
+  }
+
+  /* 
+   * Atualizar status de match para identificar que foram procurados.  
+   */
+
+  for(const i in data) {
+    const newInstanceOfMatch = await Match.findOne({where: {id: data[i].id}});
+    newInstanceOfMatch.status = 1;
+    await newInstanceOfMatch.save();
   }
 
 };
 
-const createMatchTeam = async(id, campeonato, data ) => {
+const createMatchTeam = async (id, campeonato, data) => {
 
   data.forEach(async team => {
     const newMatch = MatchTeams.build({
-      match_id: id, 
+      match_id: id,
       name: team.teamName,
       campeonato_classificacao: campeonato,
-      points: team.points, 
+      points: team.points,
       playedMatches: team.playedMatches,
       victory: team.victory,
       draw: team.draw,
@@ -36,7 +53,7 @@ const createMatchTeam = async(id, campeonato, data ) => {
       lastFive: team.lastFive
     });
 
-    try{
+    try {
       await newMatch.save();
     } catch (error) {
       console.log('Erro ao tentar salvar dados dos times das partidas', error);
